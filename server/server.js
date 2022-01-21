2c5c821b3f9a469d3cefa550818195b2da9f51aa
david fiattarone <Fiattarone@me.com>
2022-01-20 16:19:21 -0800

    Last commit before deploy: added heroku procfile

--- server/server.js
+++ server/server.js
@@ -169,11 +169,6 @@ passport.use(new LinkedInStrategy({
 }, function(accessToken, refreshToken, profile, done) {
   // asynchronous verification, for effect...
   process.nextTick(function () {
-    // To keep the example simple, the user's LinkedIn profile is returned to
-    // represent the logged-in user. In a typical application, you would want
-    // to associate the LinkedIn account with a user record in your database,
-    // and return that user instead.
-    // return done(null, profile);
     User.findOrCreate({ linkedInId: profile.id}, function(err, user) {
           if (err) { return done(err); }
           done(null, user);
